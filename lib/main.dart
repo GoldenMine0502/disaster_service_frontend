25e1270675c0d4df124d38ba63a29328bb08c755
GoldenMine0502 <ehe123@naver.com>
2024-10-29 04:55:09 +0900

    왜 메인함수에서 다른 파일 호출하면 앱이 실행이 안됨? ㅋ

--- lib/main.dart
+++ lib/main.dart
@@ -118,3 +118,66 @@ class HomePageState extends State<HomePage> {
     );
   }
 }
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 3), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MainScreen()),
+        );
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            logoBig,
+            const SizedBox(height: 20), // 이미지와 텍스트 사이 간격 조정
+            const Text(
+              "충리미와 재난 대처해요!",
+              style: TextStyle(fontSize: 20, fontFamily: 'SBAggro'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 25, right: 40),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            logoSmall,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() async {
+  await initializeNaverMap();
+  debugPrint('naver map init');
+  runApp(MaterialApp(
+    home: SplashScreen(),
+    // home: MainScreen(),
+  ));
+}
+
+// flutter build ios -t lib/splash.dart
+// flutter build run --release
