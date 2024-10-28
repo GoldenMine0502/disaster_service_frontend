4e940cf42295f01352a19881dd5c2e5e2a3faee8
GoldenMine0502 <ehe123@naver.com>
2024-10-29 04:32:40 +0900

    url 변경

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,3 +1,3 @@
 
-// const domain = 'http://home.goldenmine.kr:8081';
-const domain = 'http://192.168.50.236:8081';
+const domain = 'http://home.goldenmine.kr:8081';
+// const domain = 'http://192.168.50.236:8081';
